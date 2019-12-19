a57e2d454c9da8ad2889a909dd7afca221a60cae
UsmanAshraf1988 <u.ashraf1988@gmail.com>
2019-12-19 09:22:16 +0100

    Update shm_reader_process.c
    refactored

--- C_Development/shared_memory_IPC/read_end_proces/shm_reader_process.c
+++ C_Development/shared_memory_IPC/read_end_proces/shm_reader_process.c
@@ -1,18 +1,18 @@
-//! @file shm_header.h
+//! @file shm_reader_process.c
 //!
 //!
-//! @brief It is memory struct layout for shared memory IPC.
+//! @brief It is one end process of posix shared memory for IPC.
 //! 
 //!
-//! @author Usman Ashraf 
-//! @email  u.ashraf1988@gmail.com/usman.ashrafpak@gmail.com
+//! @author: Usman Ashraf 
+//! @email:  u.ashraf1988@gmail.com/usman.ashrafpak@gmail.com
 //!
 //! GNU Lesser General Public License v3.0, free to use, re-use, distriution and modification
 //! provided this above header information. Author take no responsibility for any outcome of
 //! this code. 
 //!
 //! If you need more information, please, contact me at my above email address.
-//
+//!
 //! Successfully executed under OS:
 //!                                RedHat 7
 //!                                RedHat 5
@@ -20,20 +20,73 @@
 //!                                Ubuntu 16
 //!                                yocto-created minimal image
 //!
+//! For debug messages, Please define this macro here.
+//! Or, at compile time with macro flags
+//! #define PROTOTYPE 1 or -DPROTOTYPE=1
 //!
 //! Compiled with GCC Version > 4.5
+//! gcc -DPROTOTYPE=1 shm_reader_process.c -o shm_reader_process -lrt -lpthread
 
 
-//! system includes
+
+#ifdef PROTOTYPE
+#define PUT_LOG(msg) if(PROTOTYPE)fprintf(stdout,"%s",msg);
+#else
+#define PUT_LOG(msg)
+#endif
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <semaphore.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#define BUF_SIZE 1024
+#include "shm_header.h"
 
-const char shm_name[] = "/shm_mem";
 
-struct Shm_Mem {
-  sem_t wrSem;
-  sem_t rdSem;
-  int cnt;
-  char buf[BUF_SIZE];
-};
+int main(int argc, char * argv []){
+
+  int fd_shm = shm_open(shm_name, O_RDWR, 0666);
+  if (fd_shm<0){
+    shm_unlink(shm_name);
+    fprintf(stderr, "shm_open:unsuccessful: \'%s\' ...\n", shm_name);
+    exit(1);
+  }
+
+  struct  Shm_Mem * shm_mem = (struct Shm_Mem *)mmap(NULL, sizeof(struct Shm_Mem), PROT_READ|PROT_WRITE,
+                                MAP_SHARED, fd_shm, 0);
+
+  if( shm_mem == ((void*)-1) ){
+    munmap(shm_mem, sizeof(struct Shm_Mem)); 
+    shm_unlink(shm_name);
+    close(fd_shm);
+    fprintf(stderr,"mmap: for posix shared memory unsuccessful...\n");
+    exit(1);
+  }
+
+  
+  while(1){
+    printf("Reader process is waiting for \'Writer process\'\n");
+    sem_wait( &(shm_mem->rdSem) );
+
+    printf("From Reader: %d characters are written, and string is: \'%s\'\n", shm_mem->cnt, shm_mem->buf);
+    if( shm_mem->cnt <= 1 ){
+      break;
+    }
+
+    sem_post( &(shm_mem->wrSem) );
+  }
+
+  //Do cleanup
+  munmap( shm_mem, sizeof(struct Shm_Mem) );
+  shm_unlink( shm_name );
+  close( fd_shm ); 
+
+
+
+  return 0;
+}
